0c6492bd702abe6c6c9a7dd440e5f69a3a3c3c76
Btopp <Niklas.Bachmann@ruhr-uni-bochum.de>
2018-02-11 17:04:31 +0100

    Refactoring.

--- Assets/TileBasedGeneration/Scripts/MapGenerator.cs
+++ Assets/TileBasedGeneration/Scripts/MapGenerator.cs
@@ -4,41 +4,42 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-	public int rows = 3; // min = 3
-	public int columns = 3; // min = 3
-
-	public List<BaseTile> allTiles;
-	private List<List<BaseTile>> placedTiles;
-	private List<BaseTile> acPossibleTiles;
-
-	private bool isGenerated = false;
+	[Range (3, 99)]
+	public int rows = 3, columns = 3;
 
 	[Range (0f, 1f)]
 	public float randomExit = 0.5f;
-	private Vector2 exitCoords = new Vector2();
+	private Vector2 exitCoords;
+
+	public List<BaseTile> allTiles;
+	private List<BaseTile> possibleTiles;
+	private List<List<BaseTile>> placedTiles;
 
 	private void Start () {
-		exitCoords = GenerateExitPositions (rows, columns, randomExit);
-
-		while (!isGenerated) {
-			placedTiles = new List <List<BaseTile>>();
-			GenerateMap();
-			Debug.Log ("Generated!");
-		}
+		GenerateExitPositions(rows, columns, randomExit);
+		GenerateMap(rows, columns);
 	}
 
-	// Calculates exits
-	public Vector2 GenerateExitPositions (int x, int y, float normalizedRandom) {
-		Vector2 coords = new Vector2 ();
+	// Calculate exit positions
+	public void GenerateExitPositions (int rows, int columns, float normalizedRandom) {
+		rows -= 1;
+		columns -= 1;
+
 		if (normalizedRandom > 0) {
-			coords = new Vector2 (Mathf.Ceil (Random.Range ((x - (x * normalizedRandom)), (x - 1))), Mathf.Ceil (Random.Range ((y - (y * normalizedRandom)), (y - 2))));
+			exitCoords = new Vector2 (
+				Mathf.Ceil (Random.Range ((rows - (rows * normalizedRandom)), (rows - 1))),
+				Mathf.Ceil (Random.Range ((columns - (columns * normalizedRandom)), (columns - 1)))
+			);
 		} else {
-			coords = new Vector2 (x - 1, y - 1);
+			exitCoords = new Vector2 (rows - 1, columns - 1);
 		}
-		return coords;
 	}
 
-	void GenerateMap () {
+	void GenerateMap (int rows, int columns) {
+		rows -= 1;
+		columns -= 1;
+
+		placedTiles = new List <List<BaseTile>>();
 
 		for (int nWidth = 0; nWidth <= rows; nWidth++) {
 			placedTiles.Add(new List<BaseTile>());
@@ -47,66 +48,64 @@ public class MapGenerator : MonoBehaviour {
 
 				// First tile
 				if (nHeight == 0 && nWidth == 0) {
-					// X tile
 					placedTiles [nWidth].Add (Instantiate (allTiles [0], new Vector3 ((-10.0f * nWidth), 0.0f, (10.0f * nHeight)), Quaternion.identity));
 
 				} else {
-					acPossibleTiles = new List<BaseTile> ();
+					possibleTiles = new List<BaseTile> ();
 
 					// Last tile
 					if (nHeight == columns && nWidth == rows) {
-						acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight);
-						isGenerated = true;
+						possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight);
 
-					// Last tiles in width
+					// Last tiles in row
 					} else if (nWidth == rows) {
 						if (nHeight == 0) {
-							acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [0].gateTop && !o.gateTop && !o.gateLeft && !o.isSpecial);
-							if (acPossibleTiles.Count == 0) {
-								acPossibleTiles = allTiles.FindAll (o => o.gateBottom && o.gateRight && !o.gateTop && !o.gateLeft);
+							possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [0].gateTop && !o.gateTop && !o.gateLeft && !o.isSpecial);
+							if (possibleTiles.Count == 0) {
+								possibleTiles = allTiles.FindAll (o => o.gateBottom && o.gateRight && !o.gateTop && !o.gateLeft);
 							}
 						} else {
 							
 							// End tile top
 							if (nHeight == exitCoords.y) {
-								acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateTop);
+								possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateTop);
 							} else {
-								acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateTop);
+								possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateTop);
 							}
 						}
 
-					// Last tiles in height
+					// Last tiles in column
 					} else if (nHeight == columns) {
 						if (nWidth == 0) {
-							acPossibleTiles = allTiles.FindAll (o => o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateRight && !o.gateBottom && !o.isSpecial);
-							if (acPossibleTiles.Count == 0) {
-								acPossibleTiles = allTiles.FindAll (o => o.gateTop && o.gateLeft && !o.gateBottom && !o.gateRight);
+							possibleTiles = allTiles.FindAll (o => o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateRight && !o.gateBottom && !o.isSpecial);
+							if (possibleTiles.Count == 0) {
+								possibleTiles = allTiles.FindAll (o => o.gateTop && o.gateLeft && !o.gateBottom && !o.gateRight);
 							}
 						} else {
 							
 							// End tile right
 							if (nWidth == exitCoords.x) {
-								acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateRight);
+								possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateRight);
 							} else {
-								acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateRight);
+								possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.gateRight);
 							}
 						}
 							
 					// First tiles in height
 					} else if (nHeight == 0) {
-						acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [0].gateTop && o.gateLeft == false && !o.isSpecial);
+						possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [0].gateTop && o.gateLeft == false && !o.isSpecial);
 
 					// First tiles in width
 					} else if (nWidth == 0) {
-						acPossibleTiles = allTiles.FindAll (o => o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateBottom == false && !o.isSpecial);
+						possibleTiles = allTiles.FindAll (o => o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && o.gateBottom == false && !o.isSpecial);
 
 					// Middle tiles
 					} else {
-						acPossibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.isSpecial);
+						possibleTiles = allTiles.FindAll (o => o.gateBottom == placedTiles [nWidth - 1] [nHeight].gateTop && o.gateLeft == placedTiles [nWidth] [nHeight - 1].gateRight && !o.isSpecial);
 					}
 
 					// Instantiate tile
-					placedTiles [nWidth].Add (Instantiate (acPossibleTiles [Random.Range (0, acPossibleTiles.Count)], new Vector3 ((-10.0f * nWidth), 0.0f, (10.0f * nHeight)), Quaternion.identity));
+					placedTiles [nWidth].Add (Instantiate (possibleTiles [Random.Range (0, possibleTiles.Count)], new Vector3 ((-10.0f * nWidth), 0.0f, (10.0f * nHeight)), Quaternion.identity));
 				}
 			}
 		}
